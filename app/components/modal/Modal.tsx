7dc170a81e3edf01abcd7ad851642606613ee8ce
Brandon Fernandez <fernandez.b@dhn.com.do>
2023-06-16 17:05:00 -0400

    change modal

--- app/components/modal/Modal.tsx
+++ app/components/modal/Modal.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io"
+
 
 interface ModalProps {
   isOpen?: boolean;
@@ -54,7 +56,7 @@ const Modal: React.FC<ModalProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 outline-none bg-neutral-800/70">
+    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 outline-none bg-neutral-800/70 z-50">
       <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-fulllg:h-auto md:h-auto">
         {/* CONTENT */}
         <div
@@ -66,7 +68,22 @@ const Modal: React.FC<ModalProps> = ({
                 ${showModal ? "opacity-100" : "opacity-0"}
             `}
         >
-          <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"></div>
+          <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            {/* Header */}
+            <div
+              className="flex items-center p-6 rounded-t justify-center relative border-b[1px]"
+            >
+              <button onClick={handleClose} className="p-1 border-0 hover:opacity-70 transition absolute left-9">
+                <IoMdClose size={18} />
+              </button>
+              <div className="text-lg font-semibold">
+                {title}
+              </div>
+            </div>
+            {/* BODY */}
+            div
+              {/* BODY */}
+          </div>
         </div>
       </div>
     </div>
